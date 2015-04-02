a82ad9c0ff7a59bf1c257587f2d74be45938e8b6
oscar-franzen <p.oscar.franzen@gmail.com>
2015-04-02 19:31:53 -0400

    .

--- utils/hclust_fr_aln.R
+++ utils/hclust_fr_aln.R
@@ -4,19 +4,23 @@ args.working_dir <- args[1]
 args.input_file <- args[2]
 args.output_dir <- args[3]
 args.algo <- args[4]
+args.mode <- args[5]
 
 .libPaths(paste0(args.working_dir,"/bin/R.lib"))
 
 library(seqinr)
 
-a<-read.alignment(args.input_file,format="fasta")
-d<-dist.alignment(a,matrix="identity")
-
-# Because the returned distance is the square root of the distance
-m<-as.matrix(d)^2
+if (args.mode == "MSA") {
+	a<-read.alignment(args.input_file,format="fasta")
+	d<-dist.alignment(a,matrix="identity")
+	
+	# Because the returned distance is the square root of the distance
+	m<<-as.matrix(d)^2
+}else if (args.mode == "PW") {
+	m<<-read.table(args.input_file,h=T)
+}
 
 d<-as.dist(m)
-
 z<-as.matrix(d)
 
 # Workaround for NaN
@@ -31,5 +35,5 @@ for (dist in c(0.005,1:10/100)) {
     df<-as.data.frame(cl)
     df<-data.frame(read_ID=rownames(df),cluster=df$cl)
 
-    write.table(df, file=paste0(args.output_dir,"/MSA.",args.algo,".",dist,".hclust"), col.names=T, row.names=F, quote=F)
+    write.table(df, file=paste0(args.output_dir,"/",args.mode,".",args.algo,".",dist,".hclust"), col.names=T, row.names=F, quote=F)
 }
